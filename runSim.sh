9df47b5199c5848b9f8bdce7c37c9e9560ba4e46
Udai <udai.arneja11ua@gmail.com>
2022-06-09 15:22:18 +0100

    Initial Commit (proper)

---
+++ runSim.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+osascript -e 'tell app "Terminal"
+    do script "FYPC2 && python3 server.py"
+    delay 13
+    do script "FYPC2 && python3 client.py"
+    do script "FYPC2 && python3 client.py"
+    do script "FYPC2 && python3 client.py"
+    do script "FYPC2 && python3 client.py"
+    do script "FYPC2 && python3 client.py"
+end tell'
+
+'tell application "Terminal" to if it is running then quit'
